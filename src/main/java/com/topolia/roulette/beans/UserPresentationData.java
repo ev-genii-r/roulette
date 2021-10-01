a24e05778b5b4c26569186b721bb18defd59fa1f
ev-genii-r <ev_genii_r@mail.ru>
2021-10-01 22:00:42 +0300

    version 1.0

--- src/main/java/com/topolia/roulette/beans/UserPresentationData.java
+++ src/main/java/com/topolia/roulette/beans/UserPresentationData.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 public class UserPresentationData {
     
     private int id;
+
     private String name;
 
     private String picture;//include only URL
--- src/main/java/com/topolia/roulette/controllers/GameCatalogController.java
+++ src/main/java/com/topolia/roulette/controllers/GameCatalogController.java
@@ -5,24 +5,29 @@ import com.topolia.roulette.service.UserPresentationDataGetter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * controller for game catalog page
  * @author Evgenii Rudkovskii 
  * @version 1.0
- * @// TODO: 01.10.2021 postMapping with buttons: all games, all pages 
+ * @// TODO: 01.10.2021 buttons: all games
+ * @// TODO: 01.10.2021 creat all post fields 
  */
 @Controller
 public class GameCatalogController {
 
     @GetMapping("/game-catalog")
-    public String greeting(Model model) {
+    public String loadCatalogGet(Model model) {
         UserPresentationDataGetter getter = new UserPresentationDataGetter();
         UserPresentationData user = new UserPresentationData();
         user = getter.testPossibility(2);
         model.addAttribute("name", user.getName());
         model.addAttribute("id", user.getId());
-        return "gameCatalog";
+        return "game-catalog";
     }
-
+    @PostMapping("/game-catalog")
+    public String loadCatalogPost(Model model){
+        return  "game-catalog";
+    }
 }
